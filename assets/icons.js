9f02539bc8fa62d1f09402c8e132bc3b6137c296
mina ehab <minasupers88@gmail.com>
2024-10-21 23:05:53 +0400

    changed icons

--- assets/icons.js
+++ assets/icons.js
@@ -1,9 +1,9 @@
-import { AntDesign, Feather, MaterialIcons } from "@expo/vector-icons";
+import { AntDesign, Feather, MaterialIcons, FontAwesome5, SimpleLineIcons } from "@expo/vector-icons";
 
 export const icons = {
     Home: (props) => <AntDesign name="home" size={26} {...props} />,
     Booking: (props) => <AntDesign name="calendar" size={26} {...props} />,
-    Rewards: (props) => <MaterialIcons name="star" size={26} {...props} />,
-    Calculator: (props) => <MaterialIcons name="calculate" size={26} {...props} />,
+    Rewards: (props) => <AntDesign name="gift" size={26} {...props} />,
+    Calculator: (props) => <SimpleLineIcons name="calculator" size={26} {...props} />,
     Cart: (props) => <Feather name="shopping-cart" size={26} {...props} />,
 };
